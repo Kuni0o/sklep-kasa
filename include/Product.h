c543494cae9603b9cb22f89848ba5e99a215ff87
Kuni0o <ckuniszyk@yahoo.com>
2025-01-09 11:22:23 +0100

    stworzenie klasy product

---
+++ include/Product.h
@@ -0,0 +1,12 @@
+#ifndef SKLEP_KASA_PRODUCT_H
+#define SKLEP_KASA_PRODUCT_H
+
+#include "string"
+
+class Product{
+private:
+    double unitPrice;
+    std::string code;
+    std::string name;
+};
+#endif //SKLEP_KASA_PRODUCT_H
